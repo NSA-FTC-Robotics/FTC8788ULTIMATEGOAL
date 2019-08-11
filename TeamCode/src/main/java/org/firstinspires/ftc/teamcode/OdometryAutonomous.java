c893801777ebfd9e97c0cea4a5459abdaa5c21d0
Jack Hutchinson <jack.hutchinson.23435@gmail.com>
2019-08-11 19:35:11 -0400

    Commented out classes not being imported and suppressed AndroidStudio warnings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryAutonomous.java
@@ -1,16 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+//import com.qualcomm.robotcore.hardware.DcMotorController;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-
+@SuppressWarnings("FieldCanBeLocal")
 //@Disabled
 public abstract class OdometryAutonomous extends LinearOpMode
 {
