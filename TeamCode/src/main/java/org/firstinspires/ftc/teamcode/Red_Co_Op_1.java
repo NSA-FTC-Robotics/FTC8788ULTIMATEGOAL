14cb496f45ebb78f7f562f37d7d277542383cb5f
Emma <carlsone20@nsacademy.org>
2019-10-30 15:55:09 -0400

    Merge remote-tracking branch 'remotes/origin/autonomous' into Emma
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Co_Op_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Co_Op_1.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-public class Red_Co_Op_1 {
+
+
+public class Red_Co_Op_1 extends OdometryAutonomous
+{
+
+    public void runOpMode()
+    {
+        
+    }
+
+
 }
